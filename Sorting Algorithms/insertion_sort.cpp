09c21f0d7568406b3acb8a7f4e52ed94e75857ee
Aryan Tele <telearyan12@gmail.com>
2023-01-02 23:07:45 +0530

    quick sort

--- Sorting Algorithms/insertion_sort.cpp
+++ Sorting Algorithms/insertion_sort.cpp
@@ -15,17 +15,7 @@ int main()
     for (int i = 1; i < n; i++)
     {
         temp = arr[i];
-        // for (int j = i - 1; j >= 0; j--)
-        // {
-        //     if (arr[j] > temp)
-        //     {
-        //         arr[j + 1] = arr[j];
-        //     }
-        //     else
-        //     {
-        //         break;
-        //     }
-        // }
+
         int j = i - 1;
         while (j >= 0)
         {
